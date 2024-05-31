2da1297f99bf6331bbd54247eb3972818c7ee16c
artbutakov <art.butakov@gmail.com>
2024-05-31 17:34:56 +0300

    Task 3 refactored

--- 3.py
+++ 3.py
@@ -1,3 +1,3 @@
 a = [[1, 2, 3], [4, 5, 6]]
-b = [{f'k{index + 1}': array[index] for index in range(len(array))} for array in a]
+b = [{f'k{index + 1}': value for index, value in enumerate(array)} for array in a]
 print(b)
