b922f365aeb796e8319010b8c91b0ab916694273
artbutakov <art.butakov@gmail.com>
2024-05-31 17:44:47 +0300

    Task 5 refactored

--- 5/task.py
+++ 5/task.py
@@ -10,9 +10,9 @@ def glue_nouns(user_noun: str = input('Enter noun: ')):
     for index, noun in enumerate(nouns):
         if index < len(nouns) - 1 and user_noun != noun:
             match = SequenceMatcher(a=user_noun, b=noun).find_longest_match()
-            sequence = user_noun[match.a:match.a + match.size]
+            sequence = noun[:match.size]
             if match.size > 1 and user_noun.endswith(sequence) and noun.startswith(sequence):
-                gotten_nouns.append(user_noun[:-match.size] + noun)
+                gotten_nouns.append(user_noun + noun[match.size:])
     return gotten_nouns
 
 
