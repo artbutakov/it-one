b9e36858341984ff01f062f475f67d0d0dfd6c77
artbutakov <art.butakov@gmail.com>
2024-06-02 15:12:11 +0300

    Task 6 refactored

--- 6/task.py
+++ 6/task.py
@@ -12,7 +12,9 @@ response = requests.get(
     params={'documents_date': int(time.time())},
 )
 data = JsonDataSchema(**response.json())
-data_frame = (pd.DataFrame(columns=data.columns, data=data.rows)
-              .rename(columns={'key1': 'document_id', 'key2': 'document_dt', 'key3': 'document_name'})
-              .assign(load_dt=datetime.now()))
+data_frame = (
+    pd.DataFrame(columns=data.columns, data=data.rows)
+    .rename(columns={'key1': 'document_id', 'key2': 'document_dt', 'key3': 'document_name'})
+    .assign(load_dt=datetime.now())
+)
 print(data_frame.to_dict('records'))
